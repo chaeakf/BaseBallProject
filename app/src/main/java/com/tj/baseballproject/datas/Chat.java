58f48ba3f392273fa17ec717d3d2a3a851d1a28a
Chae <chaeakf12@gmail.com>
2019-05-18 16:32:58 +0900

    말풍선 데이터 ArrayList에 담기

---
+++ app/src/main/java/com/tj/baseballproject/datas/Chat.java
@@ -0,0 +1,13 @@
+package com.tj.baseballproject.datas;
+
+public class Chat {
+    public boolean userSaid;
+    private String message;
+
+    public Chat(boolean userSaid, String message) {
+        this.userSaid = userSaid;
+        this.message = message;
+    }
+}
+
+
